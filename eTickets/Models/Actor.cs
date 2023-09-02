e9b0b485d263cb974a2ac38ede945a31bd85e8e2
= <=>
2023-09-02 17:17:18 +0000

    Added Actor model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,10 @@
+﻿namespace eTickets.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Bio { get; set;}
+    }
+}
